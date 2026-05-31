eade8e254c3a08dd2901e66b9eb1b9631c1eacde
Olivier Heidemann <heidemann@gmail.com>
2026-05-31 18:29:01 +0200

    Add essential CLI tools and apps to install script
    - CLI: gh, fd, fzf, ast-grep, jq, ripgrep, ncdu
    - replace iterm2 cask with ghostty (the terminal actually in use)
    - add font-fira-code-nerd-font (needed for Starship glyphs), zed, obsidian, logseq
    - fix deprecated homebrew/cask-fonts/ tap prefix on inconsolata fonts

--- apps/install.sh
+++ apps/install.sh
@@ -11,7 +11,21 @@ brew install jenv
 brew install httpie
 brew install npm
 
-brew install --cask iterm2
-brew install --cask homebrew/cask-fonts/font-inconsolata
-brew install --cask homebrew/cask-fonts/font-inconsolata-dz-for-powerline
+# CLI tools
+brew install gh
+brew install fd
+brew install fzf
+brew install ast-grep
+brew install jq
+brew install ripgrep
+brew install ncdu
+
+# Casks
+brew install --cask ghostty
+brew install --cask font-fira-code-nerd-font
+brew install --cask font-inconsolata
+brew install --cask font-inconsolata-dz-for-powerline
 brew install --cask rectangle
+brew install --cask zed
+brew install --cask obsidian
+brew install --cask logseq
